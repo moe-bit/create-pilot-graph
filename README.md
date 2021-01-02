a13d3f1fcfe6881dcd0e8e5b251eb033789142cd
Moritz Engel <moritz.engel@bdsu.de>
2021-01-02 23:55:43 +0100

    Add a function to check if usp exists

--- README.md
+++ README.md
@@ -10,8 +10,12 @@ create pilot graph is a project of the WOC 2020/21 combining Microsoft Graph API
 1. [X] ~~Connect to MS Graph API~~
 1. [X] ~~Create Useraccount~~
 1. [X] ~~Lizenz einem User zuweisen~~
+1. [X] ~~Generate a password~~
+1. [X] ~~check if userPrincipalName already exists~~
 1. [ ] Location für Lizenzzuweisung per PATCH setzen
+1. [ ] Überprüfen welche Parameter beim erstellen des Users ausgefüllt seien müssen (private Mail für e-Mail Versand/ JobTitle/ preferedLanguage)
 1. [ ] CSV einlesen
+1. [ ] CSV validieren -> passt Vorname/ Nachname/ private E-Mail Format 
 1. [ ] Einladungsmail versenden
 1. [ ] Neu angelegte User zu Gruppen hinzufügen
 1. [ ] UI für Import + ini erstellen
@@ -20,7 +24,8 @@ create pilot graph is a project of the WOC 2020/21 combining Microsoft Graph API
 ## Standard Functions:
 | Function | Description | Link |
 | ------ | ------ | ------ |
-| Test | Test | Test |  
+| createUser | Creates a Azure AD user | [Link]() |
+  
 
 
 ## Project setup
@@ -28,3 +33,9 @@ create pilot graph is a project of the WOC 2020/21 combining Microsoft Graph API
 
 ## Useful links
 Links that helped us:
+
+[Get Users](https://docs.microsoft.com/de-de/graph/api/user-list?view=graph-rest-1.0&tabs=http#code-try-29)
+[Query parameter](https://docs.microsoft.com/en-us/graph/query-parameters?context=graph%2Fapi%2F1.0&view=graph-rest-1.0#filter-parameter)
+[Use count in query](https://developer.microsoft.com/en-us/office/blogs/build-advanced-queries-with-count-filter-search-and-orderby/)
+[Acess values in json python](https://stackoverflow.com/questions/11241583/python-accessing-data-in-json-object)
+
--- creat_user.py
+++ creat_user.py
@@ -2,6 +2,8 @@ import requests
 import json
 import pandas as pd
 import configparser
+import secrets
+import string
 
 # Read in config data
 config = configparser.ConfigParser()
@@ -12,13 +14,15 @@ if 'DEFAULT' in config:
     tenant_id = str(config.get('DEFAULT', 'TENANT_ID', fallback="There is no tenant id"))
     #test_id = str(config.get('DEFAULT', 'TEST_ID', fallback="There is no test id"))
 
-    admin_mail = str(config.get('TEST_DATA', 'ADMIN_MAIL', fallback="There is no admin mail"))
+    admin_mail = str(config.get('TEST_DATA', 'ADMIN_MAIL', fallback="There is no test admin mail"))
+    domain = str(config.get('TEST_DATA', 'DOMAIN', fallback="There is no test domain"))
+    jobTitle = str(config.get('TEST_DATA', 'JOBTITLE', fallback="There is no test jobTitle"))
 else:
     print('DEFAULT is not found')
 
 
 
-#Get token to sign in as application
+# Get token to sign in as application
 token_url = 'https://login.microsoftonline.com/{}/oauth2/v2.0/token'.format(tenant_id)
 token_data = {
  'grant_type': 'client_credentials',
@@ -30,31 +34,88 @@ token_r = requests.post(token_url, data=token_data)
 token = token_r.json().get('access_token')
 
 headers = {
- 'Authorization': 'Bearer {}'.format(token)
+ 'Authorization': 'Bearer {}'.format(token),
+ # needed for counting
+ 'ConsistencyLevel' : 'eventual'
 }
 
-def create_user(surname, givenname, mailsufix, newUserPassword):
-    userDisplayname = "{a_given} {a_surname}".format(a_given=givenname,a_surname=surname)
-    # create user account
-    user_information = {
-    "accountEnabled": True,
-    "displayName": "Test User",
-    "mailNickname": "testuser",
-    "userPrincipalName": "testuser{}".format(mailsufix) ,
-    "passwordProfile" : {
-        "forceChangePasswordNextSignIn": False,
-        "password": newUserPassword
+# Generate a secure password
+def generatePassword():
+    possibleCharc = string.ascii_letters + string.digits + string.punctuation
+    # for a 20-character password
+    password = ''.join(secrets.choice(possibleCharc) for i in range(20))
+
+    return password
+
+# Checks if a given name matches requierements and format it nicely
+def checkName(name):
+    correctName = name.title()
+    return correctName
+
+# Create a username
+def createUserName(surname, givenname):
+    username = '{person_givenname}.{person_surname}'.format(person_givenname=givenname, person_surname=surname)
+    return username
+
+# Creat UserPrincipalName 
+def create_UsP(surname, givenname, domain):
+    username = createUserName(surname, givenname)
+    lowerUsername = username.lower()
+    usp = '{uspPrefix}{domain}'.format(uspPrefix=lowerUsername, domain=domain)
+
+    return usp
+
+# Check if username already exists
+def uspExist(userPrincipalName):
+    # instead of using v1.0 in query you need to use beta to be able to count result
+    users_url = 'https://graph.microsoft.com/beta/users?$count=true&$filter=startswith(userPrincipalName, \'{}\' )'.format(userPrincipalName)
+    user_response_data = json.loads(requests.get(users_url, headers=headers).text)
+    
+    # check if result is > 0
+    return user_response_data['@odata.count'] > 0
+
+
+# Creat Azure AD User
+def create_user(surname, givenname, domain):
+    correctSur = checkName(surname)
+    correctGiven = checkName (givenname)
+    userPrincipalName = create_UsP(correctSur, correctGiven, domain)
+
+    if uspExist(userPrincipalName):
+        print("usp = {} already exists!".format(userPrincipalName))
+    
+    else:
+        mailNickname = createUserName(correctSur, correctGiven)
+        createdPassword = generatePassword()
+
+        userDisplayName = "{person_given} {person_surname}".format(person_given=correctGiven, person_surname=correctSur)
+        
+        user_information = {
+        "accountEnabled": True,
+        "surName" : correctSur,
+        "givenName" : correctGiven,
+        "displayName": userDisplayName,
+        "mailNickname": mailNickname,
+        #"jobTitle": jobTitle,
+        "mail" : userPrincipalName,
+        "userPrincipalName": userPrincipalName,
+        "passwordProfile" : {
+            "forceChangePasswordNextSignIn": False,
+            "password": createdPassword
+            }
         }
-    }
+        
+        # Request to create user
+        graph_url = 'https://graph.microsoft.com/v1.0/users/'
+        create_result = requests.post(graph_url, headers=headers, json=user_information)
+        create_response = create_result.json()
+        #print(create_response)
+        
+        user_id = create_response['id']
 
-    #graph_url = 'https://graph.microsoft.com/v1.0/users/'
-    #create_result = requests.post(graph_url, headers=headers, json=user_information)
-    #create_response = create_result.json()
-    #print(create_response)
-    print(userDisplayname)
-    #user_id = create_response.json().get('id')
+        print(user_id)
 
-    assign_licence("fef3ca2d-1aae-488a-bb7b-15f234b8c2c9")
+        #assign_licence(user_id)
 
 def assign_licence(user_id):
     licence_information = {
@@ -72,4 +133,14 @@ def assign_licence(user_id):
     licence_response = licence_result.json()                                                        #Office Location setzten: https://docs.microsoft.com/de-de/archive/blogs/dsadsi/did-you-get-a-license-assignment-cannot-be-done-for-user-with-invalid-usage-location-error-when-applying-a-license-via-graph-api
     print(licence_response)
 
-create_user("test", "Stefan", "@M365x293953.onmicrosoft.com", "ewtwerwrw123!!")
+
+
+    
+    
+    
+
+
+create_user("test08", "Stefan", domain)
+#uspExist('stefan.test02@M365x293953.onmicrosoft.com')
+#print(uspExist('s'))
+#print(uspExist('y'))
