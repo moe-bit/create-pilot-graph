5ac83c2e83202f7979f81d591343eba0df70cfec
moe-bit <54353111+moe-bit@users.noreply.github.com>
2021-01-16 13:12:03 +0100

    Feature/createUser (#3)
    * Add a function to check if usp exists
    
    * Update README.md
    
    * Add function to send mail
    
    * Update README.md
    
    * Add csv import
    
    Co-authored-by: Moritz Engel <moritz.engel@bdsu.de>

--- README.md
+++ README.md
@@ -10,21 +10,38 @@ create pilot graph is a project of the WOC 2020/21 combining Microsoft Graph API
 1. [X] ~~Connect to MS Graph API~~
 1. [X] ~~Create Useraccount~~
 1. [X] ~~Lizenz einem User zuweisen~~
-1. [ ] Location für Lizenzzuweisung per PATCH setzen
+1. [X] ~~Generate a password~~
+1. [X] ~~check if userPrincipalName already exists~~
+1. [X] ~~Location für Lizenzzuweisung per PATCH setzen~~
+1. [X] ~~Einladungsmail versenden~~
+1. [ ] Überprüfen welche Parameter beim erstellen des Users ausgefüllt seien müssen (private Mail für e-Mail Versand/ JobTitle/ preferedLanguage)
 1. [ ] CSV einlesen
-1. [ ] Einladungsmail versenden
+1. [ ] CSV validieren -> passt Vorname/ Nachname/ private E-Mail Format
+1. [ ] Code aufhübschen 
 1. [ ] Neu angelegte User zu Gruppen hinzufügen
 1. [ ] UI für Import + ini erstellen
 
 
-## Standard Functions:
+## Standard API Functions:
 | Function | Description | Link |
 | ------ | ------ | ------ |
-| Test | Test | Test |  
+| createUser | Creates a Azure AD user | [Link]() |
+| sendInvitationMail| Sends a mail from an application. You need to set an account as a sender | [Link]() |
+  
 
 
 ## Project setup
 
 
+
 ## Useful links
 Links that helped us:
+
+[Get Users](https://docs.microsoft.com/de-de/graph/api/user-list?view=graph-rest-1.0&tabs=http#code-try-29)
+
+[Query parameter](https://docs.microsoft.com/en-us/graph/query-parameters?context=graph%2Fapi%2F1.0&view=graph-rest-1.0#filter-parameter)
+
+[Use count in query](https://developer.microsoft.com/en-us/office/blogs/build-advanced-queries-with-count-filter-search-and-orderby/)
+
+[Acess values in json python](https://stackoverflow.com/questions/11241583/python-accessing-data-in-json-object)
+
--- creat_user.py
+++ creat_user.py
@@ -2,23 +2,28 @@ import requests
 import json
 import pandas as pd
 import configparser
+import secrets
+import string
+import csv 
 
 # Read in config data
 config = configparser.ConfigParser()
 config.read('ms_graph.ini')
-if 'DEFAULT' in config:
-    app_id = str(config.get('DEFAULT', 'APP_ID', fallback="There is no app id"))
-    client_secret = str(config.get('DEFAULT', 'CLIENT_SECRET', fallback="There is no client secret"))
-    tenant_id = str(config.get('DEFAULT', 'TENANT_ID', fallback="There is no tenant id"))
-    #test_id = str(config.get('DEFAULT', 'TEST_ID', fallback="There is no test id"))
-
-    admin_mail = str(config.get('TEST_DATA', 'ADMIN_MAIL', fallback="There is no admin mail"))
+if 'CONNECTION' in config:
+    app_id = str(config.get('CONNECTION', 'APP_ID', fallback="There is no app id"))
+    client_secret = str(config.get('CONNECTION', 'CLIENT_SECRET', fallback="There is no client secret"))
+    tenant_id = str(config.get('CONNECTION', 'TENANT_ID', fallback="There is no tenant id"))
+    
+    admin_mail = str(config.get('TEST_DATA', 'ADMIN_MAIL', fallback="There is no test admin mail"))
+    domain = str(config.get('TEST_DATA', 'DOMAIN', fallback="There is no test domain"))
+    invitor = str(config.get('TEST_DATA', 'INVITOR', fallback="There is no test invitor"))
+    csv_path = str(config.get('TEST_DATA', 'CSV_PATH', fallback="There is no path"))
 else:
-    print('DEFAULT is not found')
+    print('CONNECTION parameters are not found')
 
 
 
-#Get token to sign in as application
+# Get token to sign in as application
 token_url = 'https://login.microsoftonline.com/{}/oauth2/v2.0/token'.format(tenant_id)
 token_data = {
  'grant_type': 'client_credentials',
@@ -30,32 +35,92 @@ token_r = requests.post(token_url, data=token_data)
 token = token_r.json().get('access_token')
 
 headers = {
- 'Authorization': 'Bearer {}'.format(token)
+ 'Authorization': 'Bearer {}'.format(token),
+ # needed for counting
+ 'ConsistencyLevel' : 'eventual'
 }
 
-def create_user(surname, givenname, mailsufix, newUserPassword):
-    userDisplayname = "{a_given} {a_surname}".format(a_given=givenname,a_surname=surname)
-    # create user account
-    user_information = {
-    "accountEnabled": True,
-    "displayName": "Test User",
-    "mailNickname": "testuser",
-    "userPrincipalName": "testuser{}".format(mailsufix) ,
-    "passwordProfile" : {
-        "forceChangePasswordNextSignIn": False,
-        "password": newUserPassword
+# Generate a secure password
+def generatePassword():
+    possibleCharc = string.ascii_letters + string.digits + string.punctuation
+    # for a 20-character password
+    password = ''.join(secrets.choice(possibleCharc) for i in range(20))
+
+    return password
+
+# Checks if a given name matches requierements and format it nicely
+def checkName(name):
+    correctName = name.title()
+    return correctName
+
+# Create a username
+def createUserName(surname, givenname):
+    username = '{person_givenname}.{person_surname}'.format(person_givenname=givenname, person_surname=surname)
+    return username
+
+# Creat UserPrincipalName 
+def create_UsP(surname, givenname, domain):
+    username = createUserName(surname, givenname)
+    lowerUsername = username.lower()
+    usp = '{uspPrefix}{domain}'.format(uspPrefix=lowerUsername, domain=domain)
+
+    return usp
+
+# Check if username already exists
+def uspExist(userPrincipalName):
+    # instead of using v1.0 in query you need to use beta to be able to count result
+    users_url = 'https://graph.microsoft.com/beta/users?$count=true&$filter=startswith(userPrincipalName, \'{}\' )'.format(userPrincipalName)
+    user_response_data = json.loads(requests.get(users_url, headers=headers).text)
+    
+    # check if result is > 0
+    return user_response_data['@odata.count'] > 0
+
+# Creat Azure AD User
+def create_user(surname, givenname, domain, privateMail):
+    correctSur = checkName(surname)
+    correctGiven = checkName (givenname)
+    userPrincipalName = create_UsP(correctSur, correctGiven, domain)
+
+    if uspExist(userPrincipalName):
+        print("usp = {} already exists!".format(userPrincipalName))
+    
+    else:
+        mailNickname = createUserName(correctSur, correctGiven)
+        createdPassword = generatePassword()
+
+        userDisplayName = "{person_given} {person_surname}".format(person_given=correctGiven, person_surname=correctSur)
+        
+        user_information = {
+        "accountEnabled": True,
+        "surName" : correctSur,
+        "givenName" : correctGiven,
+        "displayName": userDisplayName,
+        "mailNickname": mailNickname,
+        "mail" : userPrincipalName,
+        "userPrincipalName": userPrincipalName,
+        "usageLocation": "DE",
+        "passwordProfile" : {
+            "forceChangePasswordNextSignIn": False,
+            "password": createdPassword
+            }
         }
-    }
+        
+        # Request to create user
+        graph_url = 'https://graph.microsoft.com/v1.0/users/'
+        create_result = requests.post(graph_url, headers=headers, json=user_information)
+        create_response = create_result.json()
+        print(create_response)
+        
+        user_id = create_response['id']
 
-    #graph_url = 'https://graph.microsoft.com/v1.0/users/'
-    #create_result = requests.post(graph_url, headers=headers, json=user_information)
-    #create_response = create_result.json()
-    #print(create_response)
-    print(userDisplayname)
-    #user_id = create_response.json().get('id')
+        print(user_id)
 
-    assign_licence("fef3ca2d-1aae-488a-bb7b-15f234b8c2c9")
+        if assign_licence(user_id):
+            sendInvitationMail(privateMail, createdPassword, correctGiven, userPrincipalName)
+        else:
+            print("No Mail sended!")
 
+# Assing licence to a user: It's important to set the usage location before
 def assign_licence(user_id):
     licence_information = {
     "addLicenses": [
@@ -69,7 +134,64 @@ def assign_licence(user_id):
 
     licence_url = 'https://graph.microsoft.com/v1.0/users/{id}/assignLicense'.format(id=user_id)
     licence_result = requests.post(licence_url, headers=headers, json=licence_information)
-    licence_response = licence_result.json()                                                        #Office Location setzten: https://docs.microsoft.com/de-de/archive/blogs/dsadsi/did-you-get-a-license-assignment-cannot-be-done-for-user-with-invalid-usage-location-error-when-applying-a-license-via-graph-api
+    licence_response = licence_result.json()                                                        
+    
     print(licence_response)
+    return True
 
-create_user("test", "Stefan", "@M365x293953.onmicrosoft.com", "ewtwerwrw123!!")
+# The application sends an invitation mail from a specified account to the new account 
+def sendInvitationMail(privateMail, password, givenName, userPrincipalName):
+    htmlMail = """\
+        <html>
+            <body>
+            <p>Hallo {name},</p>
+            <p>wir d&uuml;rfen dich herzlich bei uns willkommen hei&szlig;en. Hier sind deine Zugangsdaten:</p>
+            <p>&nbsp;</p>
+            <p><strong>Benutzername</strong>: {username} </p>
+            <p><strong>Passwort</strong>: {userPwd} </p>
+            <p>&nbsp;</p>
+            <p>Du kannst dich hier anmelden: <a href="http://www.office.com">www.office.com</a></p>
+            <p>&nbsp;</p>
+            <p>Gru&szlig;,</p>
+            <p>Bereichsleitung IT</p>
+            </body>
+        </html>
+        """.format(name=givenName, username=userPrincipalName, userPwd=password)
+    
+    
+    mail = {
+    "message": {
+        "subject": "Testmail",
+        "body": {
+        "contentType": "html",
+        "content": htmlMail
+        },
+        "toRecipients": [
+        {
+            "emailAddress": {
+            "address": privateMail
+            }
+        }
+        ],
+    },
+    "saveToSentItems": "false"
+    }   
+
+    mailGraphUrl = 'https://graph.microsoft.com/v1.0/users/{}/sendMail'.format(invitor)
+    mailResult = requests.post(mailGraphUrl, headers=headers, json=mail)
+    print(mailResult)
+
+
+
+
+# Test output
+#create_user("test12", "Stefan", domain)
+#uspExist('stefan.test02@M365x293953.onmicrosoft.com')
+#print(uspExist('s'))
+#print(uspExist('y'))
+
+with open(csv_path, mode='r', encoding='utf-8-sig') as csv_file:
+    csv_reader = csv.DictReader(csv_file, delimiter=';')
+
+    for row in csv_reader:
+        create_user(row['surname'], row['givenname'], domain, row['privatemail'])
